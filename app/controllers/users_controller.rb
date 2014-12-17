cfd07533399fbbedde98a2bdf1de59333a106004
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2014-12-17 16:26:34 -0600

    Fixed google maps icons, they are now clickable when sorted by Sports

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 	
 	def index
+		@games = Game.all
 	end
 	def show
+		@games = Game.all
 	end
 	def new
+		@games = Game.all
 	end
 	def create
+		@games = Game.all
 	end
 	def edit
+		@games = Game.all
 	end
 	def destroy
+		@games = Game.all
 	end
 end

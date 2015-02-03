b499875160c88b8784e867eb82e23f110bc2366f
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2015-02-02 20:38:15 -0600

    First Game Model rspec test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe Game do
 	describe 'email' do
 		it 'should return the email of the user that created the game' do
-			user = User.new(:id => 1, :email => "myemail@email.com")
 			game = Game.new(:title => "Game 2", :user_id => 1)
-			expect(game.title).to eq("myemail@email.com")
+			game.user = User.new(:id => 1, :email => "myemail@email.com")
+			expect(game.email).to eq("myemail@email.com")
 		end
 	end
 end

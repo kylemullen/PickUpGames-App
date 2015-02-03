4c41740829e6a27fc1487620bd56a29c70ce892e
“kylemullengit config --global user.email “kylem.mullen.com <kylem.mullen@gmail.com>
2015-02-02 20:46:35 -0600

    Second Game test

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -7,5 +7,13 @@ describe Game do
 			game.user = User.new(:id => 1, :email => "myemail@email.com")
 			expect(game.email).to eq("myemail@email.com")
 		end
+	end		
+	describe 'park_name' do
+		it 'should return the name of the park that is where the game is taking place' do
+			game = Game.new(:title => "Game 2", :court_id => 1)
+			game.court = Court.new(:id => 1, :park_id => 1)
+			game.court.park = Park.new(:id => 1, :name => "The Park")
+			expect(game.park_name).to eq("The Park")
+			end
 	end
 end
